c5049de018e989ceba2121f192f88cee4e071117
Nicolas-15 <nic.lopezp@duocuc.cl>
2026-05-10 11:07:32 -0400

    Metodo Post para infracciones en el Core Service

--- app/src/main/java/com/sifa/sifa_go/core/network/CoreApi.kt
+++ app/src/main/java/com/sifa/sifa_go/core/network/CoreApi.kt
@@ -4,7 +4,11 @@ import com.sifa.sifa_go.data.model.PlateInfoResponse
 import com.sifa.sifa_go.data.model.TipoInfraccionResponse
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.POST
 import retrofit2.http.GET
+import retrofit2.http.Body
+import com.sifa.sifa_go.data.model.InfraccionCreateRequest
+import com.sifa.sifa_go.data.model.InfraccionResponse
 import retrofit2.http.Header
 import retrofit2.http.Path
 
@@ -27,11 +31,18 @@ interface CoreApiService {
     suspend fun getAllTipoInfracciones(
         @Header("Authorization") token: String
     ): List<TipoInfraccionResponse>
+
+    // Envia los datos de la multa al Core Service a través del Gateway
+    @POST("/core/api/v1/infracciones")
+    suspend fun createInfraccion(
+        @Header("Authorization") token: String,
+        @Body request: InfraccionCreateRequest
+    ): InfraccionResponse
 }
 
 object CoreRetrofitClient {
     //private const val BASE_URL = "http://10.15.64.34:9000/"
-    private const val BASE_URL = "http://192.168.0.11:9000"
+    private const val BASE_URL = "http://192.168.1.6:9000"
 
     val apiService: CoreApiService by lazy {
         Retrofit.Builder()
