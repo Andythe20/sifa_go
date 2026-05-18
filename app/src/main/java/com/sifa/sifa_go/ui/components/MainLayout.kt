38cb36d12113458ae942c5a2c82a2515d35b1a1f
Andrés Ortega <andythe20xd@gmail.com>
2026-05-17 23:32:49 -0400

    colores en main layout y en profile screen corregidos

--- app/src/main/java/com/sifa/sifa_go/ui/components/MainLayout.kt
+++ app/src/main/java/com/sifa/sifa_go/ui/components/MainLayout.kt
@@ -9,17 +9,22 @@ import androidx.compose.material.icons.filled.Assessment
 import androidx.compose.material.icons.filled.CameraAlt
 import androidx.compose.material.icons.filled.History
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.AlertDialogDefaults.containerColor
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarDefaults
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
 
 data class BottomNavItem(
     val title: String,
@@ -50,6 +55,7 @@ fun MainLayout(
         topBar = {
             TopAppBar(
                 title = { Text(text = title) },
+                colors = TopAppBarDefaults.topAppBarColors(MaterialTheme.colorScheme.background),
                 navigationIcon = {
                     if (!isRootTab) {
                         IconButton(onClick = onBackClick) {
@@ -71,7 +77,9 @@ fun MainLayout(
             )
         },
         bottomBar = {
-            NavigationBar {
+            NavigationBar(
+                containerColor = MaterialTheme.colorScheme.background,
+            )  {
                 bottomNavItems.forEach { item ->
                     NavigationBarItem(
                         icon = { Icon(item.icon, contentDescription = item.title) },
